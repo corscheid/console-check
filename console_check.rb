2b1f0a39994d67ee94e9497e682519b682638839
Corey Scheideman <corscheid@gmail.com>
2017-10-07 20:35:53 -0400

    display which urls invalid, minor cleanup

--- console_check.rb
+++ console_check.rb
@@ -1,5 +1,4 @@
 require 'selenium-webdriver'
-# require 'open-uri'
 
 def setup
   @driver = Selenium::WebDriver.for :chrome
@@ -17,22 +16,24 @@ end
 
 def url_data(url_filename)
   File.foreach(url_filename) do |test_url|
-    # skip the line if it's a malformed URL TODO: gather these and report them
+    # skip the line if it's a malformed URL
     if test_url !~ /^((http|https):\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/ix
-      puts '[INVALID URL]'
+      puts "[INVALID URL] : #{test_url}"
       next
-    end # if
+    end
+
     @driver.navigate.to test_url
+
     # print out js console errors
     puts test_url
     80.times { print '-' }
     puts
-    sleep(1)
+    sleep(2) # time for page loads
     errs = @driver.manage.logs.get :browser
     puts errs.empty? ? 'NO JS CONSOLE ERRORS' : errs
     80.times { print '-' }
     puts
-  end # each
+  end # foreach
 end # def
 
 run { url_data 'urls.txt' }
